0434803ba2b707aab10f3006f724b74296be5fac
Elias Calagiu <eliasc81@gmail.com>
2021-01-14 10:43:36 -0800

    basic app done

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+// const PORT = 3000
+// set Port to this when using Heroku
+
+const PORT = process.env.PORT||3000
+
+const turtles = [{
+    name: "leo",
+    isTeenageMutant: true,
+}]
+
+app.get("/", (req,res)=>{
+    res.send("welcome to my site!")
+})
+app.get("/turtles", (req,res)=>{
+    res.json(turtles);
+})
+
+app.listen(PORT, function(){
+    console.log(`listening on port ${PORT}`)
+})
